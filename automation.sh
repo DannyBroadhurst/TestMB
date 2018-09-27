a7e93963f1054da6b6d9090923cdc585a778a3e1
Danny Broadhurst <42643962+DannyBroadhurst@users.noreply.github.com>
2018-09-27 10:18:35 +0100

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -127,7 +127,10 @@ cat > /etc/3cxpbx/setupconfig.xml << "<EOF>"
 <EOF>
 # Comment
 # Install 3CX
-wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
-echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | tee /etc/apt/sources.list.d/3cxpbx.list
+wget -O- http://downloads.3cx.com/downloads/3cxpbx/public.key | apt-key add -
+echo "deb http://downloads.3cx.com/downloads/3cxpbx/ /" > /etc/apt/sources.list.d/3cxpbx.list
 apt-get update
-apt-get install -y 3cxpbx
+apt-get -y install nginx
+rm -f /etc/nginx/sites-enabled/default
+systemctl reload nginx
+apt-get -y install 3cxpbx
