6e14240826570e3eade398fbe34d1c0645ffdb40
Danny Broadhurst <42643962+DannyBroadhurst@users.noreply.github.com>
2018-09-27 10:01:45 +0100

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -4,6 +4,13 @@ mkdir /etc/3cxpbx
 cd /etc/3cxpbx
 wget https://raw.githubusercontent.com/DannyBroadhurst/TestMB/master/setupconfig.xml
 
+
+cat > /etc/3cxpbx/setupconfig.xml << "<EOF>"
+<?xml version="1.0" encoding="utf-8"?>
+<ArrayOfSerializedQuestion xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+ <!--PUT CONTENTS OF SETUPCONFIG.XML HERE--> 
+<EOF>
+
 # Install 3CX
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | tee /etc/apt/sources.list.d/3cxpbx.list
